44e664bbaad9ff176f87d0a1ab129b89944af7fd
Yash Potdar <yashyogeshpotdar7@gmail.com>
2024-12-12 00:56:30 +0530

    Updating selenium_setup.py

--- src/utils/selenium_setup.py
+++ src/utils/selenium_setup.py
@@ -1,29 +1,15 @@
-# src\utils\selenium_setup.py
-"""
-Sets up a Selenium Chrome driver with the specified options.
-
-Args:
-    headless (bool): If True, the Chrome browser will run in headless mode (without a GUI).
-
-Returns:
-    webdriver.Chrome: A Selenium Chrome driver instance with the specified options.
-"""
-
+# src/utils/selenium_setup.py
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-def setup_driver(headless=True):
-    chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    if headless:
-        chrome_options.add_argument("--headless")
-    
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=chrome_options
-    )
 
-    return driver
+def setup_selenium_driver(headless=True):
+    options = Options()
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    if headless:
+        options.add_argument("--headless")
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
